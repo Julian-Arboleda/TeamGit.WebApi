21c2c5ae09e9aa89ba5baf3c3bf7547bb97b70ae
Julian <arboledajulian@outlook.com>
2021-03-01 18:06:00 -0500

    test

--- TeamGit.Data/Reply.cs
+++ TeamGit.Data/Reply.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TeamGit.Data
 {
-    class Replies
+   public class Reply
     {
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        [Key]
+        public int ReplyId { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public virtual List<string> listOfReplies { get; }
+        public int ohshit { get; set; }
     }
 }
